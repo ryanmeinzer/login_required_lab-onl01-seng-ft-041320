72bc3e38b616ccc1a2000bf6d6d679b72a9a8799
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-06-24 16:47:00 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,11 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+  
+  def show
+  end
+
+  def require_login
+    redirect_to controller: 'sessions', action: 'new' unless session.include? :name
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret', to: 'secrets#show'
+
+  root 'application#hello'
+
 end
